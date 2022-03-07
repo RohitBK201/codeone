9021aa1e148e64a0030c25d378f45d5193cafdf8
RohitBK201 <rohitnrdtamangBK201@gmail.com>
2022-03-07 10:11:59 +0530

    structure done

--- src/App.js
+++ src/App.js
@@ -4,23 +4,88 @@ import React from 'react';
 function App() {
 
 
-  const [book,setbook]= React.useState(1)
+  const [book,setbook]= React.useState(13)
 
-  const change = (val) =>{
+  const [pen,setpen] = React.useState(10)
+
+  const [notebook,setnotebook] = React.useState(44)
+
+  const [inkpen,setinkpen] = React.useState(78)
+
+  const changeb = (val) =>{
 
     setbook(book = book + val)
 
   }
 
+  const changep = (val) =>{
+
+    setpen(pen = pen + val)
+
+  }
+  const changen = (val) =>{
+
+    setnotebook(notebook = notebook + val)
+
+  }
+
+  const changei = (val) =>{
+
+    setinkpen(inkpen = inkpen + val)
+
+  }
+
   return (
-    <div className="items">
+   <div>
+      <div className="items">
       <span>Books : </span>
+      <br></br>
 
-      <button className='addbook' onClick={ () => change(1)}>+</button><button className='rembook' onClick={ () => change(-1)}>-</button>
+      <button className='addBook' onClick={ () => changeb(1)}>+</button>
+      <button className='remBook' onClick={ () => changeb(-1)}>-</button>
 
+      <br></br>
       <span className='totalbooks'>{book}</span>
       
     </div>
+
+    <div className="items">
+      <span>Pens : </span>
+      <br></br>
+
+      <button className='addPen' onClick={ () => changep(1)}>+</button>
+      <button className='remPen' onClick={ () => changep(-1)}>-</button>
+
+      <br></br>
+      <span className='totalPens'>{pen}</span>
+      
+    </div>
+
+    <div className="items">
+      <span>Notebooks : </span>
+      <br></br>
+
+      <button className='addNotebook' onClick={ () => changen(1)}>+</button>
+      <button className='remNotebook' onClick={ () => changen(-1)}>-</button>
+
+      <br></br>
+      <span className='totalNotebooks'>{notebook}</span>
+      
+    </div>
+
+    <div className="items">
+      <span>InkPens : </span>
+      <br></br>
+
+      <button className='addInkpen' onClick={ () => changei(1)}>+</button>
+      <button className='remInkpen' onClick={ () => changei(-1)}>-</button>
+
+      <br></br>
+      <span className='totalInkpens'>{inkpen}</span>
+      
+    </div>
+   </div>
+    
   );
 }
 
