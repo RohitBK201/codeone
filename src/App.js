51fd7e5f6f910b6f4c9264d136acea6fc9f336ec
RohitBK201 <rohitnrdtamangBK201@gmail.com>
2022-03-07 09:49:43 +0530

    added buttons

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ function App() {
 
   const [book,setbook]= React.useState(1)
 
+  const change = (val) =>{
+
+    setbook(book = book + val)
+
+  }
+
   return (
     <div className="items">
       <span>Books : </span>
 
-      <button className='addbook'>+</button><button className='rembook'>-</button>
+      <button className='addbook' onClick={ () => change(1)}>+</button><button className='rembook' onClick={ () => change(-1)}>-</button>
 
       <span className='totalbooks'>{book}</span>
       
